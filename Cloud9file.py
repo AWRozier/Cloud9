cf6cbf25d3c6f1eb0b069e4ca4afb3e5a1ef60d5
AlexusRozier <alexuswoods@gmail.com>
2022-12-29 01:32:22 +0000

    Commit

---
+++ Cloud9file.py
@@ -0,0 +1,17 @@
+"""
+Your module description
+"""
+print("For the purpose of this Project, I will print out a ")
+print("The below program demonstrates how to declare empty list: 'string_list=[]' ")
+string_list = []
+print(" The below function will add items to the list: 'string_list.append'")
+
+string_list = ["EC2", "S3", "Elastic Cache", "Cloud9", "VPC", "SNS","RDS"]
+string_list.append("Cloudfront")
+print(string_list)
+print("The length of the list is:", len(string_list))
+string_list.remove("S3")
+print(string_list)
+string_list.remove("VPC")
+print(string_list)
+print("The length of the list is:", len(string_list))
